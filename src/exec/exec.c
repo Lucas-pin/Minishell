e5a3717e679aee2a3aa10014c97e892baf077f06
Lucas Pin <lucasfacundopin@gmail.com>
2025-06-26 22:09:34 +0200

    [FEATURE] Se añaden multiples pruebas unitarias para cada funcionalidad

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lucas <lucas@student.42.fr>                +#+  +:+       +#+        */
+/*   By: lpin <lpin@student.42malaga.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 18:41:38 by lpin              #+#    #+#             */
-/*   Updated: 2025/05/19 00:16:44 by lucas            ###   ########.fr       */
+/*   Updated: 2025/05/30 18:26:01 by lpin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,41 +35,87 @@ exit	Salir de la shell
 // generar buble sort
 // imprimir lista, si es env no imprime los hide
 
-int	main(int argc, char **argv, char **env)
+int main(int argc, char **argv, char **envp)
 {
-	t_env *_env;
-	char *pwd;
-	char *prueba[] = {"export", "a=b", "pepito=convalor", "pepitoo", NULL};
-	char *prueba_2[] = {"export", NULL};
-	pwd = NULL;
-	if (env == NULL)
-	{
-    	pwd = getenv("PWD");
-		printf("pwd: %s", pwd);
-	}
-	if (argc > 1)
-	{
-		printf("El primer argumento es: %s\n", argv[1]);
-	}
-	else
-	{
-		printf("No se han pasado argumentos.\n");
-	}
-	printf("-------------------Se imprime el entorno--------------------\n");
-	built_env(env, &_env);
-	printf("-------------------Se añadieron variables-------------------------\n");
-	built_export(&_env, prueba);
-	printf("-------------------Se imprime el export-------------------------\n");
-	built_export(&_env, prueba_2);
-	printf("-------------------Se actualiza variables-------------------------\n");
-	prueba[0] = "export";
-	prueba[1] = "a=c";
-	prueba[2] = "pepito";
-	prueba[3] = "pepitoo=updated";
-	prueba[4] = NULL;
-	built_export(&_env, prueba);
-	printf("-------------------Se imprime el export-------------------------\n");
-	built_export(&_env, prueba_2);
-	//Revisar que ocurre al eliminar el valor de una variable
-	return (0);
+    t_env *_env = NULL;
+    char *export1[] = {"export", "a=1", "b=2", "c=3", "d=4", "e=5", NULL};
+    char *export2[] = {"export", NULL};
+    char *unset_b[] = {"unset", "b", NULL};
+    char *unset_a[] = {"unset", "a", NULL};
+    char *unset_c[] = {"unset", "c=3", NULL};
+    char *unset_d[] = {"unset", "d=3", NULL};
+    char *unset_e[] = {"unset", "e=", NULL};
+    char *unset_d_e[] = {"unset", "d=4", "e=5", NULL};
+    char *unset_noexiste[] = {"unset", "noexiste", NULL};
+
+    // Inicializar entorno
+    built_env(envp, &_env);
+
+    // Añadir variables
+    built_export(&_env, export1);
+    printf("Lista tras export a=1 b=2 c=3:\n");
+    built_export(&_env, export2);
+    printf("\n");
+    // Eliminar 'b'
+    built_unset(&_env, unset_b);
+    printf("Tras built_unset 'b':\n");
+    built_export(&_env, export2);
+    printf("\n");
+
+    // Eliminar 'a'
+    built_unset(&_env, unset_a);
+    printf("Tras built_unset 'a':\n");
+    built_export(&_env, export2);
+    printf("\n");
+
+    // Eliminar 'c'
+    built_unset(&_env, unset_c);
+    printf("Tras built_unset 'c':\n");
+    built_export(&_env, export2);
+    printf("\n");
+
+    // Eliminar 'd' con valor erroneo
+    built_unset(&_env, unset_d);
+    printf("Tras built_unset 'd' con value erroneo:\n");
+    built_export(&_env, export2);
+    printf("\n");
+
+    // Eliminar 'e' con valor erroneo
+    built_unset(&_env, unset_e);
+    printf("Tras built_unset 'd' con value erroneo:\n");
+    built_export(&_env, export2);
+    printf("\n");
+    
+    // Eliminar variable que no existe
+    built_unset(&_env, unset_noexiste);
+    printf("Tras built_unset 'noexiste' (no debería cambiar nada):\n");
+    built_export(&_env, export2);
+    printf("\n");
+
+    // Eliminar 'd' y 'e' con valores correctos
+    built_unset(&_env, unset_d_e);
+    printf("Tras built_unset 'd=4' y 'e=5':\n");
+    built_export(&_env, export2);
+    printf("\n");
+
+    // Pruebas para built_cd
+    char cwd[1024];
+    printf("Directorio actual inicial: %s\n", getcwd(cwd, sizeof(cwd)));
+
+    // Cambiar a /tmp (o cualquier directorio que exista en tu sistema)
+    char *cd_tmp[] = {"cd", "/tmp", NULL};
+    built_cd(&_env, cd_tmp);
+    printf("Directorio tras built_cd /tmp: %s\n", getcwd(cwd, sizeof(cwd)));
+
+    // Cambiar a un directorio inválido
+    char *cd_invalido[] = {"cd", "/noexiste", NULL};
+    built_cd(&_env, cd_invalido);
+    printf("Directorio tras built_cd /noexiste (debe fallar y quedarse igual): %s\n", getcwd(cwd, sizeof(cwd)));
+
+    // Cambiar a HOME (sin argumentos)
+    char *cd_home[] = {"cd", NULL};
+    built_cd(&_env, cd_home);
+    printf("Directorio tras built_cd (HOME): %s\n", getcwd(cwd, sizeof(cwd)));
+
+    return 0;
 }
