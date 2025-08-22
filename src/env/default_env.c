8af9735844280e0f471a9ffb91a4a76c6961c36c
Lucas Pin <lucasfacundopin@gmail.com>
2025-08-22 10:30:20 +0200

    [REFACTOR] Se refactoriza la creación del environment
    - Si se recibe el environment se transforma de char ** a la estructura t_env
    - Si no se recibe un environment se crea uno por default
    - Se añade la variable $? para almacenar el estado de las ejecuciones

---
+++ src/env/default_env.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   default_env.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpin <lpin@student.42malaga.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/20 00:14:13 by lpin              #+#    #+#             */
+/*   Updated: 2025/08/20 00:41:46 by lpin             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/builtins.h"
+
+static void create_default_env(t_env **_env)
+{
+	char *pwd;
+	char *oldpwd;
+	char *pwd_var;
+	char *oldpwd_var;
+
+	pwd = getcwd(NULL, 0);  // malloc automático
+	if (!pwd)
+		pwd = ft_strdup("/");  // fallback si getcwd falla
+	oldpwd = getcwd(NULL, 0);  // otra llamada independiente
+	if (!oldpwd)
+		oldpwd = ft_strdup("/");  // fallback
+	pwd_var = ft_strjoin_free_s2("PWD=", pwd);
+	oldpwd_var = ft_strjoin_free_s2("OLDPWD=", oldpwd);
+	lst_add(_env, lst_new(DEFAULT_PATH, false));	
+	lst_add(_env, lst_new(pwd_var, false));
+	lst_add(_env, lst_new(oldpwd_var, false));
+	lst_add(_env, lst_new("SHLVL=1", false));
+	lst_add(_env, lst_new("?=0", true));
+	free(pwd_var);
+	free(oldpwd_var);
+}
+
+static void bump_shlvl(t_env **_env)
+{
+	t_env *var;
+	char  *num;
+	char  *var_value;
+	int    lvl;
+	
+	var = find_key(_env, "SHLVL=");
+	if (var && var->value)
+	{
+		lvl = ft_atoi(var->value) + 1;
+		num = ft_itoa(lvl);
+		if (!num)
+			return;
+		var_value = ft_strjoin("SHLVL=", num);
+		if (var_value)
+		{
+			update_value(_env, var_value);
+			free(var_value);
+		}
+		free(num);
+	}
+}
+
+static void ensure_question_hidden(t_env **_env)
+{
+	if (!find_key(_env, "?="))
+		lst_add(_env, lst_new("?=0", true));
+}
+
+/* Convierte envp (char**) a t_env**. Si envp es NULL/vacío -> create_default_env */
+int envp_to_env(t_env **_env, char **envp)
+{
+	int i;
+
+	if (!_env)
+		return (-1);
+	*_env = NULL;
+	if (!envp || !envp[0])
+	{
+		create_default_env(_env);
+		return (0);
+	}
+	i = 0;
+	while (envp[i])
+	{
+		lst_add(_env, lst_new(envp[i], false));
+		i++;
+	}
+	/* Ajustes de shell al iniciar sobre un env heredado */
+	bump_shlvl(_env);            /* SHLVL++ (o SHLVL=1 si faltaba) */
+	ensure_question_hidden(_env); /* Asegura que ?=0 esté presente */
+	return (0);
+}
--- src/utils/export_utils.c
+++ src/utils/export_utils.c
@@ -6,62 +6,11 @@
 /*   By: lpin <lpin@student.42malaga.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 23:33:41 by lucas             #+#    #+#             */
-/*   Updated: 2025/08/01 20:45:33 by lpin             ###   ########.fr       */
+/*   Updated: 2025/08/20 00:18:27 by lpin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/builtins_utils.h"
-
-void create_default_env(t_env **_env)
-{
-	char *pwd;
-	char *oldpwd;
-	int level;
-	char *new_shlvl;
-	char *pwd_var;
-	char *oldpwd_var;
-
-	level = 0;
-	new_shlvl = NULL;
-	
-	// Obtener directorio actual correctamente
-	pwd = getcwd(NULL, 0);  // malloc automático
-	if (!pwd)
-		pwd = ft_strdup("/");  // fallback si getcwd falla
-	
-	oldpwd = getcwd(NULL, 0);  // otra llamada independiente
-	if (!oldpwd)
-		oldpwd = ft_strdup("/");  // fallback
-	
-	// Crear variables de entorno
-	lst_add(_env, lst_new(DEFAULT_PATH, false));
-	
-	pwd_var = ft_strjoin("PWD=", pwd);
-	lst_add(_env, lst_new(pwd_var, true));
-	free(pwd_var);
-	
-	oldpwd_var = ft_strjoin("OLDPWD=", oldpwd);
-	lst_add(_env, lst_new(oldpwd_var, false));
-	free(oldpwd_var);
-	
-	// Manejar SHLVL
-	if (find_key(_env, "SHLVL=") != NULL)
-	{
-		t_env *shlvl = find_key(_env, "SHLVL=");
-		level = ft_atoi(shlvl->value) + 1;
-		new_shlvl = ft_itoa(level);
-		free(shlvl->value);
-		shlvl->value = new_shlvl;
-	}
-	else
-		lst_add(_env, lst_new("SHLVL=1", true));
-	
-	lst_add(_env, lst_new("?=0", false));
-	
-	// Liberar memoria
-	free(pwd);
-	free(oldpwd);
-}
 
 t_env	*find_key(t_env **_env, char *var)
 {
