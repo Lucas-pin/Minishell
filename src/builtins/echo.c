367cb2f717ad7559519bed54a7fa4deedb413207
Lucas Pin <lucasfacundopin@gmail.com>
2025-08-24 19:58:53 +0200

    [REFACTOR][FEATURE] Se corrige el retorno de estados en el comando 'echo'
    - Se añade la impresión de errores en cada casüistica
    - Se amplian los valores de estados devueltos en cada caso.

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,68 +6,56 @@
 /*   By: lpin <lpin@student.42malaga.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 12:47:09 by lpin              #+#    #+#             */
-/*   Updated: 2025/08/01 19:45:02 by lpin             ###   ########.fr       */
+/*   Updated: 2025/08/24 19:58:08 by lpin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/builtins.h"
 
-static void	print_without_newline(char **argv)
+static void print_words(char **args)
 {
-	char *temp;
+	int index;
 
-	if (ft_strcmp(*argv, "") == 0 || !*argv)
-		return ;
-	temp = NULL;
-	temp = ft_strdup(*argv);
-	argv++;
-	while (*argv)
+	index = 0;
+	while (args && args[index])
 	{
-		temp = ft_strjoin_free_s1(temp, " ");
-		temp = ft_strjoin_free_s1(temp, *argv);
-		argv++;
+		if (index > 0)
+			ft_putstr_fd(" ", 1);
+		ft_putstr_fd(args[index], 1);
+		index++;
 	}
-	ft_putstr_fd(temp, 1);
-	if (temp)
-		free(temp);
 }
 
-static void	print_with_newline(char **argv)
+static int handle_option(char *arg)
 {
-	char *temp;
-
-	if (ft_strcmp(*argv, "") == 0 || !*argv)
-	{
-		printf("\n");
-		return ;
-	}
-	temp = NULL;
-	temp = ft_strdup(*argv);
-	argv++;
-	while (*argv)
-	{
-		temp = ft_strjoin_free_s1(temp, " ");
-		temp = ft_strjoin_free_s1(temp, *argv);
-		argv++;
-	}
-	ft_putendl_fd(temp, 1);
-	if (temp)
-		free(temp);
+	if (!arg)
+		return (0);
+	if (arg[0] != '-')
+		return (0);
+	if (ft_strcmp(arg, "-n") == 0)
+		return (1); /* -n válido */
+	ft_putstr_fd("echo: invalid option -- ", 2);
+	ft_putendl_fd(arg, 2);
+	return (-1); /* error opción */
 }
 
-int built_echo(char **argv, t_env **_env)
+int built_echo(char **args, t_env **_env)
 {
+	int has_n;
 
 	(void)_env;
-	if (!argv)
+	if (!args || !*args)
 		return (1);
-	argv++;
-	if (ft_strcmp(*argv, "-n") == 0)
-	{
-		argv++;
-		print_without_newline(argv);
-	}
+	/* args[0] == "echo" */
+	has_n = handle_option(args[1]);
+	if (has_n < 0)
+		return (2);
+	if (has_n == 1)
+		print_words(&args[2]);
 	else
-		print_with_newline(argv);
+	{
+		print_words(&args[1]);
+		ft_putstr_fd("\n", 1);
+	}
 	return (0);
 }
