2d2b600ee13ea504890ed0c87a51ce1c51065f80
Lucas Pin <lucasfacundopin@gmail.com>
2025-08-24 19:37:55 +0200

    [REFACTOR] Se añade la impresión de errores y se corrige el valor que se debe retornar en cada caso.

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,28 +6,64 @@
 /*   By: lpin <lpin@student.42malaga.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 20:49:34 by lucas             #+#    #+#             */
-/*   Updated: 2025/08/01 19:55:19 by lpin             ###   ########.fr       */
+/*   Updated: 2025/08/24 19:33:54 by lpin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/builtins.h"
 
-int built_env(char **args, t_env **_env)
+static int args_count(char **args)
 {
-	(void)args;
-	t_env *aux;
+	int i;
 
-	if (!_env || !*_env)
-		return (1);
-	aux = *_env;
-	while (aux)
+	i = 0;
+	if (args)
+		while (args[i])
+			i++;
+	return (i);
+}
+
+static int handle_args(char **args)
+{
+	if (!args || !args[1])
+		return (0);
+	if (args[1][0] == '-')
 	{
-		if (!aux->hide)
-		{
-			printf("%s", aux->key);
-			printf("=%s\n", aux->value);
-		}
-		aux = aux->next;
+		ft_putstr_fd("env: invalid option -- ", 2);
+		ft_putendl_fd(args[1], 2);
+		return (2);
+	}
+	ft_putstr_fd("env: ", 2);
+	ft_putstr_fd(args[1], 2);
+	ft_putendl_fd(": No such file or directory", 2);
+	return (127);
+}
+
+static int print_env_list(t_env *env)
+{
+	while (env)
+	{
+		if (!env->hide && env->key && env->value)
+			printf("%s=%s\n", env->key, env->value);
+		env = env->next;
 	}
 	return (0);
+}
+
+int built_env(char **args, t_env **_env)
+{
+	int return_code;
+
+	if (!_env || !*_env)
+	{
+		ft_putendl_fd("env: environment not initialized", 2);
+		return (1);
+	}
+	if (args_count(args) > 1)
+	{
+		return_code = handle_args(args);
+		if (return_code != 0)
+			return (return_code);
+	}
+	return (print_env_list(*_env));
 }
