2cd70b20f3923e6adcad0ffbea6fcaf9bc3a9923
Lucas Pin <lucasfacundopin@gmail.com>
2025-08-24 21:39:27 +0200

    [REFACTOR] Se elimina el manejo de argumentos, ya que smplenmente si estos son > 1 se imprime un error generico

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,27 +6,13 @@
 /*   By: lpin <lpin@student.42malaga.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 20:49:34 by lucas             #+#    #+#             */
-/*   Updated: 2025/08/24 19:46:40 by lpin             ###   ########.fr       */
+/*   Updated: 2025/08/24 21:32:29 by lpin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/builtins.h"
 
-static int handle_args(char **args)
-{
-	if (!args || !args[1])
-		return (0);
-	if (args[1][0] == '-')
-	{
-		ft_putstr_fd("env: invalid option -- ", 2);
-		ft_putendl_fd(args[1], 2);
-		return (2);
-	}
-	ft_putstr_fd("env: ", 2);
-	ft_putstr_fd(args[1], 2);
-	ft_putendl_fd(": No such file or directory", 2);
-	return (127);
-}
+/* options/utilities are not supported; only plain env is allowed */
 
 static int print_env_list(t_env *env)
 {
@@ -41,18 +27,18 @@ static int print_env_list(t_env *env)
 
 int built_env(char **args, t_env **_env)
 {
-	int return_code;
+	int argc;
 
 	if (!_env || !*_env)
 	{
 		ft_putendl_fd("env: environment not initialized", 2);
 		return (1);
 	}
-	if (args_count(args) > 1)
+	argc = args_count(args);
+	if (argc > 1)
 	{
-		return_code = handle_args(args);
-		if (return_code != 0)
-			return (return_code);
+		ft_putendl_fd("env: too many arguments", 2);
+		return (1);
 	}
 	return (print_env_list(*_env));
 }
