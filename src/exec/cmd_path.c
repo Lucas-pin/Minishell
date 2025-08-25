6163c105cd5c17242b3b983236d39012951dc3e5
Lucas Pin <lucasfacundopin@gmail.com>
2025-08-25 22:50:28 +0200

    [REFACTOR] Se modifica la lógica de cmd_path, ya que luego se controlan posibles errores en la ejecución

--- src/exec/cmd_path.c
+++ src/exec/cmd_path.c
@@ -6,13 +6,12 @@
 /*   By: lpin <lpin@student.42malaga.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/28 00:56:47 by lpin              #+#    #+#             */
-/*   Updated: 2025/08/11 01:06:25 by lpin             ###   ########.fr       */
+/*   Updated: 2025/08/25 22:47:05 by lpin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/executor.h"
 #include "../../include/builtins.h"
-#include "../../include/builtins_utils.h"
 
 static builtin_func	get_builtin_func(const t_builtin cmd)
 {
@@ -60,7 +59,7 @@ static char	**path_cleaner(t_env **env)
 	char	**raw_path;
 
 	path_env = find_key(env, "PATH=");
-	if (!path_env)
+	if (!path_env || !path_env->value || path_env->value[0] == '\0')
 		return (NULL);
 	raw_path = ft_split(path_env->value, ':');
 	if (!raw_path)
@@ -73,7 +72,16 @@ static char	*get_cmd_path(t_cmd *cmd, char **raw_path)
 	char	*cmd_path;
 
 	cmd_path = NULL;
-	if (!cmd || !cmd->cmd || !raw_path || !*raw_path)
+	if (!cmd || !cmd->cmd)
+		return (NULL);
+	/* If command contains '/', attempt to exec directly */
+	if (ft_strchr(cmd->cmd, '/'))
+	{
+		if (access(cmd->cmd, X_OK) == 0)
+			return (ft_strdup(cmd->cmd));
+		return (NULL);
+	}
+	if (!raw_path || !*raw_path)
 		return (NULL);
 	while (*raw_path)
 	{
@@ -97,11 +105,9 @@ t_cmd	*cmd_path(t_cmd *cmd, t_env **env)
 	t_cmd	*head;
 	int		builtin_index;
 
-	if (!cmd || !env || !*env || ft_strcmp(cmd->cmd, "") == 0)
+	if (!cmd || !env || ft_strcmp(cmd->cmd, "") == 0)
 		return (NULL);
 	raw_path = path_cleaner(env);
-	if (!raw_path)
-		return (NULL);
 	head = cmd;
 	while (cmd)
 	{
@@ -112,7 +118,10 @@ t_cmd	*cmd_path(t_cmd *cmd, t_env **env)
 			cmd->cmd_path = (void *)get_cmd_path(cmd, raw_path);
 		cmd = cmd->next;
 	}
-	ft_split_destroyer(raw_path);
-	free(raw_path);
+	if (raw_path)
+	{
+		ft_split_destroyer(raw_path);
+		free(raw_path);
+	}
 	return (head);
 }
