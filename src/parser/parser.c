26e52063b2efb728c514df527c84e92434ad067b
Lucas Pin <lucasfacundopin@gmail.com>
2025-08-26 16:11:03 +0200

    [FIX] Se corrgien leaks de memoria generados en parser.c debido a la implementacion de señales

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -3,32 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: manualva <manualva@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lpin <lpin@student.42malaga.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 10:50:58 by manualva          #+#    #+#             */
-/*   Updated: 2025/07/28 18:05:21 by manualva         ###   ########.fr       */
+/*   Updated: 2025/08/26 16:09:56 by lpin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-/* Processes tokens until a pipe is encountered, then creates a new command from
-the tokens. */
+/* Processes tokens until a pipe is encountered, then creates a new command
+from the tokens. */
 static void	parse_till_pipe(t_token **cur, t_token **start, t_cmd **cmds)
 {
 	t_cmd	*cmd;
-	int	 redir_status;
+	int		redir_status;
 
 	cmd = init_cmd();
 	if (!cmd)
 		return ;
 	redir_status = handle_redirection(start, cmd);
 	if (redir_status == -1)
+	{
+		free(cmd);
+		*cur = NULL;
 		return ;
+	}
 	if (redir_status == -2)
 	{
 		free(cmd);
-		free_tokens(*start);
 		*start = NULL;
 		*cur = NULL;
 		return ;
@@ -52,7 +55,10 @@ static int	handle_last_command(t_token **start, t_cmd **cmds)
 		return (0);
 	redir_status = handle_redirection(start, cmd);
 	if (redir_status == -1)
+	{
+		free(cmd);
 		return (0);
+	}
 	if (redir_status == -2)
 	{
 		free(cmd);
@@ -68,10 +74,12 @@ t_cmd	*parser(t_token *tokens)
 	t_cmd	*cmds;
 	t_token	*cur;
 	t_token	*start;
+    t_token	*head;
 
 	cmds = NULL;
 	cur = tokens;
 	start = cur;
+    head = tokens;
 	while (cur)
 	{
 		if (cur->type == T_PIPE)
@@ -85,10 +93,10 @@ t_cmd	*parser(t_token *tokens)
 	{
 		if (!handle_last_command(&start, &cmds))
 		{
-			free_tokens(start);
+	    free_tokens(head);
 			return (cmds);
 		}
 	}
-	free_tokens(start);
+    free_tokens(head);
 	return (cmds);
 }
