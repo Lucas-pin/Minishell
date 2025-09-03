fcc670e2473d861e26ea15435c2ace57e596f2fa
Lucas Pin <lucasfacundopin@gmail.com>
2025-09-03 18:45:36 +0200

    [DOCS][REFACTOR] Se refactoriza builtins_utils.c para que cumpla con la norminette

--- src/utils/builtins_utils.c
+++ src/utils/builtins_utils.c
@@ -6,15 +6,15 @@
 /*   By: lpin <lpin@student.42malaga.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/24 19:44:38 by lpin              #+#    #+#             */
-/*   Updated: 2025/08/24 19:45:34 by lpin             ###   ########.fr       */
+/*   Updated: 2025/09/03 18:45:05 by lpin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/builtins_utils.h"
 
-int args_count(char **args)
+int	args_count(char **args)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	if (args)
